5316a5b0b582bf2953a5b70187ee024e9383481d
ArtemMiro <ninovch@yandex.ru>
2024-02-12 23:52:54 +0700

    1?

--- 1.py
+++ 1.py
@@ -1,18 +1,27 @@
+def rec_minus(n, m):
+    len_n, len_m = len(str(n)), len(str(m))
+    k = len_n - len_m - 1 if len_n != len_m else 0
+    c = 0
+
+    while n >= m:
+        n -= m * 10 ** k
+        c += 1
+
+    return c
+
+
 def divide(n, m):
-    rest = 0
-    end = str()
+    end = '0'
 
-    for d in str(n):
-        d = int(d)
-        d += rest * 10
-        whole = d // m
+    while n >= m:
+        len_n, len_m = len(str(n)), len(str(m))
+        k = len_n - len_m - 1 if len_n != len_m else 0
 
-        if whole:
-            rest = d % m
-        else:
-            rest = d
+        if n - m * 10 ** k > 0:
+            c = rec_minus(n, m * 10 ** k)
+            n -= m * 10 ** k * c
+            end += str(c)
+            if n < m * 10 ** (k - 1) and k != 0:
+                end += '0'
 
-        end += str(whole)
-
-    end = int(end)
-    return end, n % end
+    return int(end)

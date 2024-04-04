ae9826a2acd276a91f9450b9351fe3431349ef82
ArtemMiro <ninovch@yandex.ru>
2024-04-04 23:31:25 +0700

    21 noooo

--- 21.py
+++ 21.py
@@ -1,5 +1,5 @@
 class BasicNode:
-    def __init__(self, priority: int, value: int, parent=None, children=None):
+    def __init__(self, priority: int = None, value: int = None, parent=None, children=None):
         self.priority = priority
         self.value = value
         self.parent = parent
@@ -23,7 +23,7 @@ class BasicNode:
 
 class BinaryHeap:
     def __init__(self, trees=None):
-        self.trees = trees if trees else []
+        self.trees = trees if trees else [BasicNode()]
 
     @staticmethod
     def __move_up(node):
@@ -52,7 +52,53 @@ class BinaryHeap:
         self.merge(BinaryHeap([BasicNode(priority, value)]))
 
     def extract_min(self):
-        pass
+        out = self.peek_min()
+        self.trees[out.size()] = None
+        if not self.trees[-1]:
+            del self.trees[-1]
+
+        if out.children:
+            self.merge(BinaryHeap(out.children))
+
+        out.parent = None
+        out.children = []
+        return out
 
     def merge(self, other):
-        pass
+        frm, to = sorted((self.trees, other.trees), key=lambda node: len(node)) # ничего не получается по схеме
+        print(frm, to)
+        r = [None] * len(to)
+        carry = None
+        for i, (f, t) in enumerate(zip(frm, to)):
+            if f and t and carry:
+                r[i] = carry
+                carry = f.merge(t)
+            elif f and t:
+                carry = f.merge(t)
+                r[i] = None
+            elif f and carry:
+                carry = f.merge(carry)
+                r[i] = None
+            elif t and carry:
+                carry = carry.merge(t)
+                r[i] = None
+            elif f:
+                r[i] = f
+            elif t:
+                r[i] = t
+            else:
+                r[i] = carry
+                carry = None
+        self.trees = carry
+
+
+from random import randint
+
+b = BinaryHeap()
+x = set([randint(0, 50) for _ in range(15)])
+print(x)
+for i in x:
+    b.insert(i, 0)
+print(b)
+print(b.extract_min())
+print(b.extract_min())

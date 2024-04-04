39af7d3e775899aba56b39a8c1266e67b806f34a
ArtemMiro <ninovch@yandex.ru>
2024-04-04 12:54:35 +0700

    14

--- 14.py
+++ 14.py
@@ -1,43 +1,37 @@
 from random import randint
 
 
-def q_sort(nums, split):
-    if len(nums) > 1:
-        j = len(nums) - 1
-        ind = randint(0, j)
-        pivot = nums[ind]
-        array[ind], array[j] = array[j], array[ind]
-        i = 1
+def q_sort(nums, split, left=None, right=None):
+    left = left if left else 0
+    right = right if right else len(nums)
 
-        while i <= j:
-            if nums[i] < pivot:
-                if nums[j] < pivot:
-                    i += 1
-                else:
-                    i += 1
-                    j -= 1
-            else:
-                if nums[j] > pivot:
-                    j -= 1
-                else:
-                    array[i], array[j] = array[j], array[i]
-                    i += 1
-                    j -= 1
+    if right <= left + 1:
+        return nums[left]
 
-        array[j], array[0] = array[0], array[j]
+    i, j = left, right - 1
 
-        if j > split:
-            q_sort(nums[:j], split)
-        elif j < split:
-            q_sort(nums[j:], split - j)
+    pivot = nums[randint(left, right - 1)]
+    while True:
+        while i <= j and nums[i] < pivot:
+            i += 1
+        while i <= j and nums[j] > pivot:
+            j -= 1
+        if i >= j:
+            break
+        nums[i], nums[j] = nums[j], nums[i]
+        i, j = i + 1, j - 1
 
-    return nums
+    if (i == split and nums[i] == pivot) or (j == split and nums[j] == pivot):
+        return pivot
+    if i > split:
+        return q_sort(nums, split, left, i)
+
+    return q_sort(nums, split, j + 1, right)
 
 
 def sol(arr):
     split = len(arr) // 2
-    arr = q_sort(arr, split)
-    return arr[split]
+    return q_sort(arr, split)
 
 
 array = [1, 14, 13, 11, 15, 1, 4, 2, 7, 11, 10, 14, 9, 10, 12]
